02f9fcfd6ec26339943312c412decee5ff1ce2b1
retuzhamidulloh <retuz416@gmail.com>
2025-10-02 15:07:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+if (!localStorage.getItem("login") && !localStorage.getItem("password")) {
+  localStorage.setItem("login", "hamidulloh");
+  localStorage.setItem("password", "300");
+}
+
+let login = document.getElementById("login");
+let password = document.getElementById("password");
+let form = document.getElementById("form");
+let glas = document.getElementById("glas");
+let errorMsg = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+ 
+  if (
+    login.value === localStorage.getItem("login") &&
+    password.value === localStorage.getItem("password")
+  ) {
+    errorMsg.textContent = "";
+    window.location.href = "abaut.html"; 
+    alert("login togri ✅");
+  } else {
+    errorMsg.textContent = "Parol yoki login noto‘g‘ri kiritilgan!";
+      alert("login yoki password hato  ❌");
+  }
+});
+
+
+glas.addEventListener("click", () => {
+  const isHidden = password.type === "password";
+  password.type = isHidden ? "text" : "password";
+  glas.textContent = isHidden ? "🙈" : "🙉";
+});
